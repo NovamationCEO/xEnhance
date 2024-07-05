03d38711bc4117b824ef9f9d4e8e5d272bcd7995
Chris Young <novamation@gmail.com>
2024-07-05 18:53:34 -0400

    Efforts to fix sliding out-of-date on 'something.'

--- ios/xEnhance/main.m
+++ ios/xEnhance/main.m
@@ -1,2 +1,8 @@
-export { default } from './debounce';
-export * from './debounce';
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
